ad8257871b1cf86e6d5be6684ec324ac7013dced
Cesar Leguizamon <fabi1816@gmail.com>
2020-07-04 18:01:52 -0300

    Es un snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tlc-lisp "1.0.0"
+(defproject tlc-lisp "1.0.0-SNAPSHOT"
   :description "Interprete de TLC-Lisp"
   :url "https://github.com/fabi1816/75.14-Lenguajes-Formales"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
