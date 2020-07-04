04b15151acc00f860e79857b7f0ef5bbcc263d54
Cesar Leguizamon <fabi1816@gmail.com>
2020-07-04 19:03:07 -0300

    Agregado el archivo para tests del TP final

---
+++ test/tlc_lisp/main_test.clj
@@ -0,0 +1,12 @@
+(ns tlc-lisp.main-test
+  (:require [clojure.test :refer :all]
+            [tlc-lisp.main :refer :all]))
+
+
+(deftest test-que-falla
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+(deftest test-que-pasa
+  (testing "Soy un genio!"
+    (is (= 1 1))))
